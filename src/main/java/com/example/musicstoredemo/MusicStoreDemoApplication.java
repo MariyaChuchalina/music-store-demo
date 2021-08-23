ae48a545021bea4e297adfeb6fa7472cf2d313a3
MariyaChuchalina <mariya.chuchalina@gmail.com>
2021-08-23 14:34:27 +0300

    Add Tomcat Dependencies

--- src/main/java/com/example/musicstoredemo/MusicStoreDemoApplication.java
+++ src/main/java/com/example/musicstoredemo/MusicStoreDemoApplication.java
@@ -2,15 +2,15 @@ package com.example.musicstoredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class MusicStoreDemoApplication {
+public class MusicStoreDemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicStoreDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStoreDemoApplication.class, args);
+    }
 
 }
